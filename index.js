12d609e72cc836705419241bc28637b9bbc6c7f1
petiteoh <nikkiteoh@gmail.com>
2020-10-08 14:49:32 -0700

    bootstrap styling

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://localhost/URL-shortener", {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     res.render("index")
 });
+
+app.post("/shortly", (req, res) => {
+
+})
 
 app.listen(process.env.PORT || 5000);
