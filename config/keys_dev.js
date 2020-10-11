21ffcf9d9fea46171a156ad2ec45432c391e9013
petiteoh <nikkiteoh@gmail.com>
2020-10-10 21:51:12 -0700

    setup mongodb

---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://admin:9IZPUUfs4bs71qjn@chimps-1-molhk.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+};
--- index.js
+++ index.js
@@ -2,21 +2,27 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Shortly = require("./models/shortly");
 const app = express();
+const server = require("http").createServer(app);
+const db = require("./config/keys.js").mongoURI;
+const port = process.env.PORT || 5000
+server.listen(port, () => console.log(`listening on port ${port}`))
 
-mongoose.connect("mongodb://localhost/URL-shortener", {
-    useNewUrlParser: true, useUnifiedTopology: true
-})
+// mongoose.connect("mongodb://localhost/URL-shortener", {
+//     useNewUrlParser: true, useUnifiedTopology: true
+// });
+
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => console.log("connect to MongoDB"))
+    .catch((err) => console.log(err));
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: false }))
 
 app.get("/", (req, res) => {
     res.render("index")
 });
 
-app.post("/shortly", async (req, res) => {
-    await Shortly.create({ original: req.body.oriURL })
-    res.redirect("/")
+app.post("/shortly", (req, res) => {
+    Shortly.create({ original: req.body.oriURL })
 })
 
 app.listen(process.env.PORT || 5000);
