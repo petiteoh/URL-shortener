879eb39fee283e6fd35f66bc07baed8fe08cac26
petiteoh <nikkiteoh@gmail.com>
2020-10-12 12:07:51 -0700

    cache urls

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
-// module.exports = {
-//     mongoURI: "mongodb+srv://admin:9IZPUUfs4bs71qjn@chimps-1-molhk.mongodb.net/test?retryWrites=true&w=majority",
-//     secretOrKey: "secret"
-// };
+module.exports = {
+  mongoURI: "mongodb+srv://dev:PF24pdrddnqpFw7e@url-shortener.mhlm0.mongodb.net/URL-shortner?retryWrites=true&w=majority",
+  secretOrKey: "secret",
+};
--- index.js
+++ index.js
@@ -3,26 +3,29 @@ const mongoose = require("mongoose");
 const Shortly = require("./models/shortly");
 const app = express();
 const server = require("http").createServer(app);
-const db = require("./config/keys.js").mongoURI;
+const db = require("./config/keys_dev").mongoURI;
 const port = process.env.PORT || 5000
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 server.listen(port, () => console.log(`listening on port ${port}`))
 
-// mongoose.connect("mongodb://localhost/URL-shortener", {
-//     useNewUrlParser: true, useUnifiedTopology: true
-// });
-
 mongoose.connect(db, { useNewUrlParser: true })
-    .then(() => console.log("connect to MongoDB"))
-    .catch((err) => console.log(err));
+    .then(() => console.log("connected to MongoDB"))
+    .catch((err) => console.log(err)
+);
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-    res.render("index")
+app.get("/", async (req, res) => {
+    debugger
+    const shortlys = await Shortly.find();
+    res.render("index", { shortlys: shortlys })
 });
 
-app.post("/shortly", (req, res) => {
-    Shortly.create({ original: req.body.oriURL })
-})
-
-app.listen(process.env.PORT || 5000);
+app.post("/shortly", async (req, res) => {
+    debugger
+    await Shortly.create({ original: req.body.oriURL })
+});
