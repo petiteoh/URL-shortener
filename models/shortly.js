c2769fefd47db06e93a87bb1c006affa9fb07850
petiteoh <nikkiteoh@gmail.com>
2020-10-12 12:35:32 -0700

    add timestamps

--- models/shortly.js
+++ models/shortly.js
@@ -2,21 +2,22 @@ const mongoose = require("mongoose");
 const shortId = require("shortid");
 
 const shortlySchema = new mongoose.Schema({
-    original: {
-        type: String,
-        required: true,
-    },
-    short: {
-        type: String,
-        required: true,
-        default: shortId.generate,
-    },
-    clicks: {
-        type: Number,
-        required: true,
-        default: 0,
-    },
-    timestamps: true
+  original: {
+    type: String,
+    required: true,
+    timestamps: true,
+  },
+  short: {
+    type: String,
+    required: true,
+    default: shortId.generate,
+    timestamps: true,
+  },
+  clicks: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
 });
 
 module.exports = mongoose.model("Shortly", shortlySchema)
