25d8a8c7800b300165cc3489eaa185b74b7c6ee5
petiteoh <nikkiteoh@gmail.com>
2020-10-12 12:25:31 -0700

    add timestamps

--- models/shortly.js
+++ models/shortly.js
@@ -15,7 +15,8 @@ const shortlySchema = new mongoose.Schema({
         type: Number,
         required: true,
         default: 0,
-    }
+    },
+    timestamps: true
 });
 
 module.exports = mongoose.model("Shortly", shortlySchema)
