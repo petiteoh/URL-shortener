b677297304b9712f690320080af43e208041d69a
petiteoh <nikkiteoh@gmail.com>
2020-10-11 20:32:21 -0700

    update module keys

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
-module.exports = {
-    mongoURI: "mongodb+srv://admin:9IZPUUfs4bs71qjn@chimps-1-molhk.mongodb.net/test?retryWrites=true&w=majority",
-    secretOrKey: "secret"
-};
+// module.exports = {
+//     mongoURI: "mongodb+srv://admin:9IZPUUfs4bs71qjn@chimps-1-molhk.mongodb.net/test?retryWrites=true&w=majority",
+//     secretOrKey: "secret"
+// };
